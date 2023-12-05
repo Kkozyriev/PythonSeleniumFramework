30f7ceb65cebb72c6c39a584c5e978f0badfa4b0
Kkozyriev <konstanty.kozyriew@gmail.com>
2023-12-05 18:40:14 +0100

    Created a file for working with databases

---
+++ framework/src/helpers/database_helpers.py
@@ -0,0 +1,34 @@
+import pymysql
+from framework.src.helpers.config_helpers import get_database_credentials
+from framework.src.configs.GeneralConfigs import GeneralConfigs
+
+def read_from_db(sql):
+    db_creds = get_database_credentials()
+
+    # connect to database
+    connection = pymysql.connect(host=db_creds['db_host'], port=db_creds['db_port'],
+                                 user=db_creds['db_user'], password=db_creds['db_password'])
+    # read from the database
+    try:
+        cursor = connection.cursor(pymysql.cursors.DictCursor)
+        cursor.execute(sql)
+        db_data = cursor.fetchall()
+        cursor.close()
+    finally:
+        connection.close()
+
+    # return the result
+    return db_data
+
+
+def get_order_from_db_by_order_no(order_no):
+
+    schema = GeneralConfigs.DATABASE_SCHEMA
+    table_prefix = GeneralConfigs.DATABASE_TABLE_PREFIX
+
+    sql = f"SELECT * FROM {schema}.{table_prefix}posts WHERE ID={order_no} AND post_type = 'shop_order_placehold';"
+
+    db_order = read_from_db(sql)
+
+    return db_order
+
