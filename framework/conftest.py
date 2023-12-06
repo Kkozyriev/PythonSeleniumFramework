3afd386f8887ea0a3061fe05635f3e211fecc516
Kkozyriev <konstanty.kozyriew@gmail.com>
2023-12-06 22:17:57 +0100

    HTML Reports Creator w/ screenshots is added

--- framework/conftest.py
+++ framework/conftest.py
@@ -3,7 +3,7 @@ from selenium.webdriver.chrome.options import Options as ChOptions
 from selenium.webdriver.firefox.options import Options as FFoptions
 import pytest
 import os
-import time
+import pytest_html
 
 
 @pytest.fixture(scope="class")
@@ -47,4 +47,29 @@ def init_driver(request):
     yield
     driver.quit()
 
+
+@pytest.hookimpl(hookwrapper=True)
+def pytest_runtest_makereport(item, call):
+    outcome = yield
+    report = outcome.get_result()
+    extras = getattr(report, "extras", [])
+    if report.when == "call":
+        # always add url to report
+        xfail = hasattr(report, "wasxfail")
+        # check if test failed
+        if (report.skipped and xfail) or (report.failed and not xfail):
+            is_frontend_test = True if 'init_driver' in item.fixturenames else False
+            if is_frontend_test:
+                results_dir = os.environ.get("RESULT_DIR")
+                if not results_dir:
+                    raise Exception("Environment variable 'RESULT_DIR' must be set.")
+                screenshot_path = os.path.join(results_dir, item.name + '.png')
+                driver_fixture = item.funcargs['request']
+                driver_fixture.cls.driver.save_screenshot(screenshot_path)
+
+
+                # only add additional html on failure
+                extras.append(pytest_html.extras.image(screenshot_path))
+
+        report.extras = extras
 
